7929e000ae5d4dfc2fa99e86ac26bfbf71346265
wonkgb0922 <wonkgb0922@gmail.com>
2023-09-19 12:03:39 +0900

    9/19

--- week1/11399.cpp
+++ week1/11399.cpp
@@ -6,18 +6,18 @@
 using namespace std;
 
 int main() {
-	int n, in, sum = 0, res = 0;
-	vector<int> v;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &in);
-		v.push_back(in);
+    int n, in, sum = 0, res = 0;
+    vector<int> v;
+    cin >> n;
+        for (int i = 0; i < n; i++) {
+	    scanf("%d", &in);
+	    v.push_back(in);
 	}
 	sort(v.begin(), v.end());
 	for (int i = 0; i < n; i++) {
-		sum += v[i];
-		res += sum;
+	    sum += v[i];
+	    res += sum;
 	}
-	cout << res;
-	return 0;
+    cout << res;
+    return 0;
 }
