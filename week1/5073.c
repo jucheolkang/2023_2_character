ab998835a4c5bb9052b404a6b61872ee6f1dd2e5
maxJung <maxjung9299@gmail.com>
2023-09-22 21:02:07 +0900

    9/18

--- week1/5073.c
+++ week1/5073.c
@@ -6,17 +6,8 @@ int main() {
         scanf("%d %d %d", &a, &b, &c);
         if (a == 0 && b == 0 && c == 0) break;
         if (a == b && b == c) printf("Equilateral\n");
-        else if (a == b && b != c) printf("Isosceles\n");
-        else if (b == c && c != a) printf("Isosceles\n");
-        else if (c == a && a != b) printf("Isosceles\n");
-        else {
-            if (a < b && b < c && (a + b) <= c) printf("Invalid\n");
-            else if (b < c && c < a && (b + c) <= a) printf("Invalid\n");
-            else if (c < a && a < b && (c + a) <= b) printf("Invalid\n");
-            else if (b < a && a < c && (b + a) <= c) printf("Invalid\n");
-            else if (c < b && b < a && (c + b) <= a) printf("Invalid\n");
-            else if (a < c && c < b && (a + c) <= b) printf("Invalid\n");
-            else printf("Scalene\n");
-        }
+        else if (a >= b + c || b >= a + c || c >= a + b) printf("Invalid\n");
+        else if (a == b || b == c || c == a) printf("Isosceles\n");
+        else printf("Scalene\n");
     }
 }
