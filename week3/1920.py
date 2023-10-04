4bc5ddcbb64afb7fcb66c81f2076668a14fe0f1a
parksy2211 <113407629+parksy2211@users.noreply.github.com>
2023-10-04 10:16:53 +0000

    10/04

---
+++ week3/1920.py
@@ -0,0 +1,13 @@
+a = int(input())
+A = list(map(int, input().split()))
+b = int(input())
+B = list(map(int, input().split()))
+card1 = set(A)
+result= []
+for i in B:
+    if i in card1:
+        result.append(1)
+    else:
+        result.append(0)
+
+print("\n".join(map(str, result)))
