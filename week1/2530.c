220d26e6efc419217e9c21352c7fd51bec477776
정명환 <maxjung7@naver.com>
2023-10-04 20:52:07 +0900

    10/04

---
+++ week1/2530.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    c += d;
+
+    b += c / 60;
+    c %= 60;
+
+    a += b / 60;
+    b %= 60;
+
+    a %= 24;
+
+    printf("%d %d %d\n", a, b, c);
+}
