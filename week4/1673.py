0bfdc3f7335b4a9dc814d0ef5cb78c0aec1d4b17
parksy2211 <113407629+parksy2211@users.noreply.github.com>
2023-11-06 19:06:23 +0900

    11/06

---
+++ week4/1673.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+try:
+    while True:
+        n, k = map(int, input().split())
+        chicken = n + (n//k)
+        while n>= k:
+        	
+            n = (n//k) + (n%k) 
+            chicken += n//k 
+        print(chicken)
+except:
+    exit()
