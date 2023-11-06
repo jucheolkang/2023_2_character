afcb18ee200daecbec102def8773b6f359137d37
mulro <aswe2004@naver.com>
2023-11-06 19:09:29 +0900

    week3

---
+++ week3/백준 1673번.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n, k, r, sum;
+
+	while (scanf_s("%d %d", &n, &k) != EOF)
+	{
+		if (k > 1 && k <= n && n <= 1000000000)
+		{
+			sum = n;
+
+			while (n >= k)
+			{
+				r = n % k;
+				n /= k;
+				sum += n;
+				n += r;
+			}
+
+			n, r, k = 0;
+
+			printf("%d\n", sum);
+		}
+	}
+	
+	return 0;
+	
+}
