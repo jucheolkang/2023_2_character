e3c3b29f8fad26e2163fae04275724829248beb6
MinGyu Kim <minku010718@gmail.com>
2023-11-06 18:16:38 +0900

    11/6

---
+++ 1673/1673.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, k;
+
+    while (cin >> n >> k) {
+        int total_chickens = n; // 초기 치킨 개수
+        int max_chickens = 0;   // 먹을 수 있는 최대 치킨 개수
+        max_chickens = n; // 처음 주문한 치킨 개수
+
+        while (total_chickens >= k) {
+
+            // k개의 도장을 모으면 치킨 쿠폰으로 교환
+            int exchanged_chickens = total_chickens / k;
+            max_chickens += exchanged_chickens; // 최대 치킨 개수 업데이트
+            // 교환한 치킨 쿠폰은 제외하고 남은 치킨 쿠폰 개수 갱신
+            total_chickens = total_chickens % k + exchanged_chickens;
+        }
+
+        cout << max_chickens << endl;
+    }
+
+    return 0;
+}
