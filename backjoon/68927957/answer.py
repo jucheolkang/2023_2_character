c033e0516abcd613e378a484972fe9e82ba7da3a
송성엽 <ssyoub0327@daum.net>
2023-11-07 15:33:53 +0900

    Update answer.py

--- backjoon/68927957/answer.py
+++ backjoon/68927957/answer.py
@@ -1,17 +1,14 @@
 while True:
     try:
         n, k = map(int, input().split())
-
         result = n
+        
         while True:
             if n<k:
                 break
-            
-            result += n//k
-            
-            n = (n%k) + (n//k)
-        
+    
+            result += n//k    
+            n = (n%k) + (n//k)    
         print(result)
-        
     except:
         break
