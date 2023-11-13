f2e8f2ec4c3a97ce51b5d16bda1222d3f9baafcb
MinGyu Kim <minku010718@gmail.com>
2023-11-13 18:36:51 +0900

    11/13

---
+++ week10/24263/24263.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n = 0;
+	cin >> n;
+	if (n)
+	{
+		cout << n << endl;
+		cout << "1" << endl;
+	}
+	else
+	{
+		cout << "4" << endl;
+	}
+}
