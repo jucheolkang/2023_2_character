bb87f4a4d009fc820b0805957abd8d52519f6c35
송성엽 <ssyoub0327@daum.net>
2023-11-13 18:30:51 +0900

    Create answer.py

---
+++ backjoon/2511/answer.py
@@ -0,0 +1,24 @@
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+a_sum, b_sum = 0, 0
+victory = 'D'
+
+for i in range(len(a)):
+    if(a[i]>b[i]):
+        a_sum += 3
+        victory = 'A'
+    elif(a[i]==b[i]):
+        a_sum += 1
+        b_sum += 1
+    else:
+        b_sum += 3
+        victory = 'B'
+
+print(a_sum, b_sum)
+
+if a == b:
+    print('D')
+elif a_sum==b_sum:
+    print(victory)
+else:
+    print('A' if a_sum>b_sum else 'B')
